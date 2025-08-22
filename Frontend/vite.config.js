635f2feb08bdeb87623cefe3ee2cec04fb62019d
Suman-2003 <sumanpreetsingh998@gmail.com>
2025-08-22 21:31:29 +0530

    Updates

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,8 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
-})
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000, // keep higher to avoid warnings
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          router: ['react-router-dom'],
+          motion: ['framer-motion'],
+          socket: ['socket.io-client'],
+          axios: ['axios'],
+        },
+      },
+    },
+  },
+});
