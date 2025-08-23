8f2f77f55e4cb1335f8a500cf87cae258e030b01
Suman Preet Singh <155362476+SumanX08@users.noreply.github.com>
2025-08-23 15:58:32 +0530

    Update config.js

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -2,4 +2,4 @@ export const API_URL =
   import.meta.env.REACT_API_URL ||
   (import.meta.env.MODE === "development"
     ? "http://localhost:5000"
-    : "https://your-backend.onrender.com");
+    : "https://connectly-backend.onrender.com");
